3b9b54610ce3d70c8993b129d5c93c0b07516b75
Joshua Howland <development@jkhowland.com>
2014-10-15 16:45:14 -0600

    Initial Commit

---
+++ SimpleDelegate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleDelegate
+//
+//  Created by Joshua Howland on 10/15/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
